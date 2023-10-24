859e05fcf2ce2fc008dbd72fa7017edbeeac05aa
Alona1994 <maslovskaphoto@gmail.com>
2023-10-24 20:36:59 +0200

    1

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
+
+  const totalWidth = contentWidth + paddingWidth + borderWidth;
+
+  return totalWidth;
+}
